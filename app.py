59afd5f7423beeadf6d94211abaddcade39e519d
Shelby Drabant <Shelby Drabant>
2020-02-04 12:47:17 -0800

    working redis db

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect
+from redis import Redis
+
+app = Flask(__name__)
+redis = Redis(host='localhost', port=6379, db=0, password=None, socket_timeout=None,)
+
+@app.route('/', methods =['POST','GET'])
+def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        color = request.form.get('color')
+        print(name)
+        print(str(color))
+        try:
+            redis.set(name, color)
+            return redirect('/')
+        except:
+            print("You messed something up")
+            print(name)
+            print(str(color))
+            return redirect('/')
+    else:
+        name = request.form['find_name']
+        print(name)
+        if name != None:
+            try:
+                print(redis.get(name))
+            except:
+                print("couldn't find that user")
+            return redirect('/')
+        return render_template('index.html')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', debug=True)
